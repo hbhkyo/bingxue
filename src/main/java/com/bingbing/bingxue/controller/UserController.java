0b069af536dad2118c0390b3269d65a646dceb64
hbhkyo <hbhkyo@163.com>
2018-09-19 15:50:30 +0800

    查询提交

--- src/main/java/com/bingbing/bingxue/controller/UserController.java
+++ src/main/java/com/bingbing/bingxue/controller/UserController.java
@@ -1,8 +1,39 @@
 package com.bingbing.bingxue.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bingbing.bingxue.common.log.Log;
+import com.bingbing.bingxue.entity.User;
+import com.bingbing.bingxue.service.UserService;
 
 @Controller
 public class UserController {
 	
+	@Resource
+	private UserService userService;
+	
+	@RequestMapping(value = "/index") 
+	public String index(){
+		User u = new User();
+		u.setMobile("18627058835");
+		userService.login(u);
+		Log.info("第一个查询");
+		return "index";
+	}
+	
+	@RequestMapping(value = "/user/login", method = RequestMethod.GET) 
+	public String login(){
+		return "login";
+	}
+	
+	@RequestMapping(value = "/user/login", method = RequestMethod.POST) 
+	public String login(@ModelAttribute User user){
+		Log.info(user.toString());
+		return "index";
+	}
 }
