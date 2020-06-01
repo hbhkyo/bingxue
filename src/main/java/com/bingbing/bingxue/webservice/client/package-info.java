5a94d0b104ab22ff8d53cf58e6124fc40a434286
hbhkyo <hbhkyo@163.com>
2020-06-01 14:19:16 +0800

    web

--- src/main/java/com/bingbing/bingxue/webservice/client/package-info.java
+++
@@ -1,8 +0,0 @@
-/**  
-* <p>Title: package-info</p>  
-* <p>Description: </p>  
-* @author Administrator    
-* @date 2019年12月13日  
-* @version 1.0  
-*/
-package com.bingbing.bingxue.webservice.client;
